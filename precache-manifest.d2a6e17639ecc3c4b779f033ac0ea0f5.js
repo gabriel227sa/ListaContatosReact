c71ca38c26bab7416c2241e1f10c27b68a0dc77d
Gabriel Araujo <gabriel227sa@gmail.com>
2020-09-22 21:13:26 -0300

    Updates

---
+++ precache-manifest.d2a6e17639ecc3c4b779f033ac0ea0f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f3345886339e40fb85f17519620871",
+    "url": "/ListaContatosReact/index.html"
+  },
+  {
+    "revision": "a8b5d846916b16d75306",
+    "url": "/ListaContatosReact/static/css/main.a81c2b05.chunk.css"
+  },
+  {
+    "revision": "8ba2b2686cb655f40d21",
+    "url": "/ListaContatosReact/static/js/2.f188a52d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ListaContatosReact/static/js/2.f188a52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b5d846916b16d75306",
+    "url": "/ListaContatosReact/static/js/main.256d1ec7.chunk.js"
+  },
+  {
+    "revision": "e8c4bc1b954fc72ab59a",
+    "url": "/ListaContatosReact/static/js/runtime-main.97813552.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/ListaContatosReact/static/media/logo.82a7c062.svg"
+  }
+]);
